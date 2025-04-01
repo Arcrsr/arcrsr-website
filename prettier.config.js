298fb3a170f28c1bdf937149d1eb91f7ed97ac8f
Lv Chen <672725321@qq.com>
2025-04-02 02:42:42 +0800

    update

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,6 @@
 /** @type {import('prettier').Config} */
 module.exports = {
     singleQuote: true,
-    printWidth: 100,
     proseWrap: 'never',
     tabWidth: 4,
     htmlWhitespaceSensitivity: 'strict',
